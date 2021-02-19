472e6fb5a09abb1470eeb72a48b4e8a062f841b2
Victor <victorfabregascastro@gmail.com>
2021-02-19 09:43:50 +0100

    .

--- home.php
+++ home.php
@@ -1,9 +1,9 @@
 <?php
-    session_start();
-    if(!isset($_SESSION["username"])){
-        header('Location: ./index.php?redirected');
-        exit;
-    }
+session_start();
+if (!isset($_SESSION["username"])) {
+    header('Location: ./index.php?redirected');
+    exit;
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,18 +12,25 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="css/.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+
     <style>
         body {
             background-image: url('./images/logo-imaginest.png');
             background-repeat: no-repeat;
             background-attachment: fixed;
-            background-position: center; 
+            background-position: center;
         }
     </style>
 </head>
 <body>
     <h2>Página HOME</h2>
-    <?php echo "<p>¡Hola " . $_SESSION["username"] . "!</p>";?>
+    <?php echo "<p>¡Hola " . $_SESSION["username"] . "!</p>"; ?>
+
     <p><a href="./logout.php">Cerrar sesión</a></p>
 </body>
 </html>
